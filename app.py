a06c69764f075885104d8020854a4153e27895d6
IntanTapiDebug <157697076+KodeTanpaBug@users.noreply.github.com>
2025-10-07 12:04:40 +0700

    app.py

--- app.py
+++ app.py
@@ -158,6 +158,5 @@ def index():
 # ===============================
 # Jalankan server
 # ===============================
-if __name__ == "__main__":
-    from os import environ
-    app.run(host='0.0.0.0', port=int(environ.get('PORT', 5000)))
+if __name__ == '__main__':
+    app.run(debug=True)
